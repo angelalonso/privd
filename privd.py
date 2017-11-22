f594a36592a59384404391d25d2c3cb3e2703956
Angel Alonso <a.fonseca@foodpanda.com>
2017-11-22 15:26:25 +0100

    first draft of an object-oriented privd

---
+++ privd.py
@@ -0,0 +1,29 @@
+# TODO:
+# Load a config file
+# If missing, create a template one (or have it ready)
+
+# OBJECTS
+# SourceFolder
+# DestinationFolder
+# File
+
+class File(object):
+  """ A file that is going to be encrypted, decrypted and synced
+  """
+
+  def __init__(self, path):
+    """ Returns a File whose path is path
+    """
+    self.path = path
+
+  def encrypt(self, key):
+    """ Returns the path to the encrypted version of the file
+    """
+    return "come"
+    
+
+
+if __name__ == "__main__":
+  newfile = File('$HOME/blah')
+  print(newfile.encrypt("blah"))
+
