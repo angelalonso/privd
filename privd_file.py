ccfe7bdaa891e523e5f3d8e244ab8affd399a536
aaf <alonsofonseca.angel@gmail.com>
2017-12-03 08:14:54 +0100

    encryption and decryption work

--- privd_file.py
+++ privd_file.py
@@ -1,5 +1,6 @@
-import subprocess
+import hashlib
 import os
+import subprocess
 
 class File(object):
   """ A file object capable of being encrypted, decrypted and synced
@@ -18,7 +19,11 @@ class File(object):
     # TODO: pass user and encryption details as a parameter 
 
     # whatever there is, remove it first
-    os.remove(file_enc)
+    print("encrypting " + file_enc)
+    self.statuscheck()
+    try:
+      os.remove(file_enc)
+    except FileNotFoundError: pass
     cmd = 'gpg -e -r ' + key.id + ' --trust-model always --output ' + file_enc + ' ' + self.path
     cmd_run = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True) 
 
@@ -26,7 +31,40 @@ class File(object):
   def decrypt(self, file_enc):
     """ Decrypts file back to original path
     """
-    #cmd = 'gpg -d --output ' + self.path + '.dec ' + file_enc
+    print("DEcrypting to " + self.path)
+    self.statuscheck()
+    try:
+      os.remove(self.path)
+    except FileNotFoundError: pass
     cmd = 'gpg -d -o ' + self.path + ' ' + file_enc
     cmd_run = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True) 
+
+
+  def statuscheck(self):
+    """ Gets the checksum  and last modification time of the file 
+
+    Hashlib part taken from https://stackoverflow.com/questions/22058048/hashing-a-file-in-python
+    """
+    # We'll read in 64kb chunks
+    BUF_SIZE = 65536
+
+    # MD5? SHA1? why not both?
+    md5 = hashlib.md5()
+    sha1 = hashlib.sha1()
+
+    with open(self.path, 'rb') as f:
+      while True:
+        data = f.read(BUF_SIZE)
+        if not data:
+          break
+        md5.update(data)
+        sha1.update(data)
+
+    print("MD5: {0}".format(md5.hexdigest()))
+    print("SHA1: {0}".format(sha1.hexdigest()))
+    
+    stats_buffer = os.stat(self.path)
+    print("Last modified: {}".format(stats_buffer.st_mtime))
+
+
     
--- privd_syncer.py
+++ privd_syncer.py
@@ -1,6 +1,7 @@
-import subprocess
-import os
 import glob
+import os
+import subprocess
+import time
 
 from privd_file import File as File
 
@@ -18,13 +19,17 @@ class Syncer(object):
     for folder in config.folders:
       self.folder_initialize(folder['path'])
       self.folder_encrypt(folder['path'], config.enc_folder, key)
+      time.sleep(1)
+
       self.folder_decrypt(folder['path'], config.enc_folder, key)
+
 
   def folder_initialize(self, path):
     if not os.path.exists(path):
       os.makedirs(path)
     elif os.path.isfile(path):
       print("Error! Folder name " + path + " already exists and it's a file")
+
 
   def folder_encrypt(self, path, enc_folder, key):
     objects = glob.glob(path + "/**/*", recursive=True)
@@ -38,7 +43,6 @@ class Syncer(object):
       enc_obj_path = obj.replace(path, enc_folder + "/" + path)
       if os.path.isfile(obj):
         managed_file = File(obj)
-        # TODO: overwrite!
         managed_file.encrypt(enc_obj_path, key)
         # TODO:
         # Check data, store it into a file AND an object
@@ -49,15 +53,22 @@ class Syncer(object):
           os.makedirs(enc_obj_path)
 
 
-  #TODO: do this actually
-  # TODO: use better var names, check if overwrite or not, create dirs
   def folder_decrypt(self, path, enc_folder, key):
-    objects = glob.glob(path + "/**/*", recursive=True)
+    objects = glob.glob(enc_folder + path + "/**/*", recursive=True)
+    print(path)
+    print(enc_folder)
 
-    # Create the folder as such within the enc folder
-    enc_path = path.replace(path, enc_folder + "/" + path)
+    # Create the folder to decrypt file to if it isn't there already
+    if not os.path.exists(path):
+      os.makedirs(path)
     for obj in objects:
-      enc_obj = obj.replace(path, enc_folder + "/" + path)
-      dec_file = File(obj)
-      dec_file.decrypt(enc_obj)
+      print(obj)
+      dec_obj_path = obj.replace(enc_folder + path, path)
+      print(dec_obj_path)
+      if os.path.isfile(obj):
+        managed_file = File(dec_obj_path)
+        managed_file.decrypt(obj)
+      elif os.path.isdir(obj):
+        if not os.path.exists(dec_obj_path):
+          os.makedirs(dec_obj_path)
       
