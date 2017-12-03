6f17aa785fe618debbf005dde3fdb2e589bd75ba
aaf <alonsofonseca.angel@gmail.com>
2017-12-03 15:01:13 +0100

    cleaning up config

--- privd.py
+++ privd.py
@@ -1,15 +1,9 @@
 # TODO:
-# create a file.encrypt and a file.decrypt 
-# do everything on a folder
 # load, create and/or correct gpg keys
 
-
-# OBJECTS
-# SourceFolder
-# DestinationFolder
-# File
-
+import argparse
 import gnupg
+import logging as log
 import time
 import subprocess
 from privd_config import Config
@@ -18,6 +12,17 @@ from privd_syncer import Syncer as Syncer
 
 
 if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('-v', '--verbose', help='Show higher level of verbosity', required=False, action='store_true')
+
+  args = vars(parser.parse_args())
+
+  if args['verbose']:
+    log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+  else:
+    log.basicConfig(format="%(levelname)s: %(message)s", level=log.INFO)
+
   config = Config()
 
   key = Key(config.key_email)
--- privd_config.py
+++ privd_config.py
@@ -1,4 +1,5 @@
 import os
+import logging as log
 import yaml
 from privd_tools import correct_path as cpath
 
@@ -8,41 +9,37 @@ class Config(object):
     """
 
     def __init__(self):
+        log.debug("Config files initiated:")
         with open("config.yaml", 'r') as stream:
             load = yaml.safe_load(stream)
-        #MAIN FOLDER
+
         try:
             self.main_folder = cpath(load['main_folder'])
         except KeyError as exc:
             self.main_folder = os.environ['HOME'] + "/.privd"
-        #MAIN FOLDER
+            log.debug("Using default Main folder")
+        log.debug("Main folder: " + self.main_folder)
+
         try:
             self.enc_folder = cpath(load['enc_folder'])
         except KeyError as exc:
             self.enc_folder = self.main_folder + "/enc"
-        #KEY EMAIL
-        try:
-            self.key_email = load['key_email']
-        except KeyError as exc:
-            self.key_email = "default@privd.foo"
-        #FOLDERS TO ENCRYPT
+            log.debug("Using default folder for encrypted files")
+        log.debug("Folder for encrypted files: " + self.enc_folder)
+
         try:
             self.folders = load['folders']
             for folder in self.folders:
               folder['path'] = cpath(folder['path'])
-
         except KeyError as exc:
             self.folders = []
+            log.debug("Empty set of Folders to encrypt")
+        log.debug("Folders to encrypt: " + str(self.folders))
 
-
-        #KEYS FOLDER
-        # NEEDED?
         try:
-            self.keys_folder = self.main_folder + load['keys_folder']
+            self.key_email = load['key_email']
         except KeyError as exc:
-            self.keys_folder = self.main_folder + "/keys"
-        # Check and create folders
-#if not os.path.exists(directory):
-#    os.makedirs(directory)
-
+            self.key_email = "default@privd.foo"
+            log.debug("Using default E-Mail for GPG Key")
+        log.debug("E-Mail for GPG Key: " + self.key_email)
 
