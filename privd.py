08211a064188e242106f6279a1c2b165abb75c97
aaf <alonsofonseca.angel@gmail.com>
2017-12-17 08:44:40 +0100

    refurbishing logging and modes

--- privd.py
+++ privd.py
@@ -1,8 +1,10 @@
 # TODO:
-# load, create and/or correct gpg keys
-# Create better different levels of logging messages (info, debug, error...)
-# USe $HOME to store, change to /home/user only when taking actions -> compatibility with Mac
 # Remove unused functions
+# USe $HOME to store, change to /home/user only when taking actions -> compatibility with Mac
+# Create better different levels of logging messages (info, debug, error...)
+# Save public, private keys on home directory
+# load, create and/or correct gpg keys
+# Add other modes encrypt, decrypt...? is this even needed?
 
 import argparse
 import gnupg
@@ -18,10 +20,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-v', '--verbose', help='Show higher level of verbosity', required=False, action='store_true')
-    parser.add_argument('-a', '--auto', help='Automatically syncs folders configured', required=False, action='store_true')
-    parser.add_argument('-d', '--daemon', help='Daemon-like run. Similar to auto but on a loop', required=False, action='store_true')
-    parser.add_argument('-e', '--encrypt', help='Encrypt the folders configured', required=False, action='store_true')
-    parser.add_argument('-u', '--unencrypt', help='Un-encrypt (Decrypt) the folders configured', required=False, action='store_true')
 
     args = vars(parser.parse_args())
 
@@ -33,27 +31,9 @@ if __name__ == "__main__":
     config = Config()
     key = Key(config.key_email)
 
-    if args['encrypt']:
-        # either syncer init initializes folders, or syncer.encrypt does
-        syncer = Syncer(config, key)
-        for folder in config.folders:
-            syncer.folder_encrypt(folder['path'], config.enc_mainfolder, key)
-    elif args['unencrypt']:
-        # either syncer init initializes folders, or syncer.encrypt does
-        syncer = Syncer(config, key)
-        for folder in config.folders:
-            syncer.folder_decrypt(folder['path'], config.enc_mainfolder, key)
-    elif args['auto']:
-        # either syncer init initializes folders, or syncer.encrypt does
-        syncer = Syncer(config, key)
-        syncer.auto()
-        syncer.refresh()
-    elif args['daemon']:
-        # either syncer init initializes folders, or syncer.encrypt does
-        syncer = Syncer(config, key)
-        while True:
-            print("#################################################################" + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) )
-            print("#################################################################" + str(int(time.time())) )
-            syncer.daemon()
-            time.sleep(4)
+    syncer = Syncer(config, key)
+    while True:
+        log.debug("########## Latest run at: " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + str(int(time.time())) )
+        syncer.daemon()
+        time.sleep(1)
   
