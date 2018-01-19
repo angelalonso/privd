4b696a8b5aa93c35a66b08e431a18e4cbabb6d01
aaf <alonsofonseca.angel@gmail.com>
2018-01-20 00:20:52 +0100

    new windowtype notification with timeout

--- window.py
+++ window.py
@@ -1,3 +1,4 @@
+from time import sleep
 import tkinter as tk
 
 class MyWindow():
@@ -6,6 +7,23 @@ class MyWindow():
         self.frame = tk.Frame(self.root)
         self.frame.pack()
         self.result = ""
+
+    def notification(self, message):
+        timeout = 5000
+        msg_width = len(message) * 14
+        btn_width = 4
+        title = tk.Message(self.frame, 
+                text = message, 
+                pady = 15, 
+                padx = 15, 
+                width = msg_width)
+        title.pack(side = tk.TOP)
+        btn_ok = tk.Button(self.frame, 
+                 width = btn_width,
+                 text = "Close", 
+                 command = lambda: self.on_button_clicked(""))
+        btn_ok.pack(side = tk.RIGHT)
+        self.root.after(timeout, lambda: self.root.destroy()) 
 
     def on_button_clicked(self, message):
         self.result = message
@@ -20,21 +38,36 @@ class MyWindow():
                 padx = 15, 
                 width = msg_width)
         title.pack(side = tk.TOP)
-        button = tk.Button(self.frame, 
+        btn_yes = tk.Button(self.frame, 
                  width = btn_width,
                  text = "Yes", 
                  command = lambda: self.on_button_clicked("yes"))
-        button.pack(side = tk.LEFT)
-        slogan = tk.Button(self.frame,
+        btn_yes.pack(side = tk.LEFT)
+        btn_no = tk.Button(self.frame,
                  width = btn_width,
                  text = "No",
                  fg = "red",
                  command = lambda: self.on_button_clicked("no"))
-        slogan.pack(side = tk.RIGHT)
+        btn_no.pack(side = tk.RIGHT)
+
+    def prepare_widget(self):
+        self.root.overrideredirect(1)
+        self.root.update_idletasks()
+        w = self.root.winfo_width() 
+        h = self.root.winfo_height()
+        ws = self.root.winfo_screenwidth() # width of the screen
+        hs = self.root.winfo_screenheight() # height of the screen
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+        self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.root.update()
 
     def show_win(self, mode, title):
         if mode == "true_false":
             result = self.true_false(title)
+        elif mode == "notification":
+            result = self.notification(title)
+        self.prepare_widget()
         self.root.mainloop()
         return self.result
 
